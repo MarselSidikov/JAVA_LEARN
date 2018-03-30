79122691fc1172d5b14b8b535f1a4ec8177b094a
Student21 <student21@itpark.ru>
2018-03-30 19:24:32 +0300

    add characters

---
+++ Themes/9. Characters/src/ru/itpark/Main.java
@@ -0,0 +1,26 @@
+package ru.itpark;
+
+public class Main {
+
+  public static void main(String[] args) {
+    char a = 'A';
+    int codeA = a;
+    System.out.println(codeA);
+    int codeB = 66;
+    char b = (char)codeB;
+    System.out.println(b);
+
+    char text[] = "Hello, 123!".toCharArray();
+
+    for (char c : text) {
+      if ('A' <= c && c <= 'Z' ||
+          'a' <= c && c <= 'z') {
+        System.out.println(c + " - буква");
+      } else if ('0' <= c && c <= '9') {
+        System.out.println(c + " - цифра");
+      } else {
+        System.out.println(c + " - что-то другое");
+      }
+    }
+  }
+}
